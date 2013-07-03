ac0995b5c3371e7e956beeeaed50b55cbebc1c45
pwierzb1 <rzbikson@gmail.com>
2013-07-03 14:00:46 +0200

    change groupId. better sample. including missing assembly.xml

--- src/main/resources/archetype-resources/src/main/resources/sample.scala
+++ src/main/resources/archetype-resources/src/main/resources/sample.scala
@@ -1,13 +1,17 @@
+import _root_.net.grinder.script.Grinder
+import _root_.net.grinder.script.Test
+import _root_.scala.Predef._
 import net.grinder.script.Test
 
-def log(msg:String) = {
-  println(msg)
-}
-
-val test = new Test(1, "test log")
-
 () => {
+  val test = new Test(1, "test log")
+  def log(msg:String) = {
+    println(msg)
+  }
+  log("start process %s" format Grinder.grinder.getProcessNumber)
   () => {
+    log("start thread %s" format Grinder.grinder.getThreadNumber)
+
     val $log = log _
     test.record($log)
     $log("test logger")
